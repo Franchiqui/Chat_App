d92d2d96f8d4ed837dba3c12c8744a15e46eb592
Francisco Javier Romero Temblador <exchiqui.fr@gmail.com>
2025-04-16 15:41:56 +0200

    visto avatares

--- lib/widgets/chat_list_item.dart
+++ lib/widgets/chat_list_item.dart
@@ -1,6 +1,7 @@
 // lib/widgets/chat_list_item.dart
 import 'package:flutter/material.dart';
 import '../models/chat_model.dart';
+import '../config/pocketbase_config.dart';
 
 class ChatListItem extends StatelessWidget {
   final ChatModel chat;
@@ -19,7 +20,21 @@ class ChatListItem extends StatelessWidget {
     // Determinar si el usuario actual es user1 o user2
     final bool isUser1 = chat.user1 == currentUserId;
     final String displayName = isUser1 ? chat.displayNameB : chat.displayNameA;
-    final String? avatarUrl = isUser1 ? chat.fotoUrlB : chat.fotoUrlA;
+    final String? rawAvatarUrl = isUser1 ? chat.fotoUrlB : chat.fotoUrlA;
+    final pb = PocketBaseConfig.pb;
+    final baseUrl = pb.baseUrl;
+
+    // Determine if the avatar URL is a full URL or just a filename/path
+    String? avatarUrl;
+    if (rawAvatarUrl != null && rawAvatarUrl.isNotEmpty) {
+      if (rawAvatarUrl.startsWith('http')) {
+        avatarUrl = rawAvatarUrl;
+      } else {
+        avatarUrl = '$baseUrl/api/files/users/${isUser1 ? chat.user2 : chat.user1}/$rawAvatarUrl';
+      }
+    }
+    debugPrint('[ChatListItem] Avatar URL: '
+        '${avatarUrl ?? "(none)"} for displayName: $displayName');
 
     return ListTile(
       leading: CircleAvatar(
